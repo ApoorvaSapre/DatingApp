22c159c5a8779b87da5b7f528706203fa0e6bbb8
ApoorvaSapre <apoorvaspr@gmail.com>
2022-09-11 22:35:53 +0530

    End of Section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,16 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Apoorva");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Prachi");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Vilas");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"Aruna");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (5,"Akshat");
